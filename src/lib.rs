1e6daaa4adb9702aa1871c57a43c8f006848b65f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-10-30 18:46:14 +1100

    Use a raw structure for custom messages

--- src/lib.rs
+++ src/lib.rs
@@ -5,40 +5,61 @@ use once_cell::sync::Lazy;
 use reqwest::Client;
 use serde::Serialize;
 use serde_json::to_string_pretty;
+use structures::WebhookMessage;
 
 static HTTP: Lazy<Client> = Lazy::new(Client::new);
 
 pub struct DiscordWebhook {
-  hook_url: Option<String>
+    hook_url: Option<String>,
 }
 
 fn stringify_struct<T: Serialize>(from: &T) -> Result<String> {
-  return Ok(to_string_pretty(from)?)
+    return Ok(to_string_pretty(from)?);
 }
 
 impl DiscordWebhook {
-  pub fn new(hook_url: String) -> Self {
-    DiscordWebhook {
-      hook_url: Some(hook_url)
-    }
-  }
-
-  pub async fn send_text(&self, message: &str) -> Result<()> {
-    if let Some(url) = &self.hook_url {
-      let wmessage = structures::WebhookMessage {
-        content: Some(message.to_owned()),
-        username: None,
-        embed: None,
-        avatar_url: None,
-        allowed_mentions: None,
-    };
-
-      let body = stringify_struct(&wmessage)?;
-
-      HTTP.post(url).header("Content-Type", "application/json").body(body).send().await?;
-      return Ok(())
+    pub fn new(hook_url: String) -> Self {
+        DiscordWebhook {
+            hook_url: Some(hook_url),
+        }
     }
 
-    Err(anyhow!("You must set the hook url via .set_url"))
-  }
+    pub async fn send_text(&self, message: &str) -> Result<()> {
+        // Make sending messages somewhat shorter
+        if let Some(url) = &self.hook_url {
+            let wmessage = structures::WebhookMessage {
+                content: Some(message.to_owned()),
+                username: None,
+                embed: None,
+                avatar_url: None,
+                allowed_mentions: None,
+            };
+
+            let body = stringify_struct(&wmessage)?;
+
+            HTTP.post(url)
+                .header("Content-Type", "application/json")
+                .body(body)
+                .send()
+                .await?;
+            return Ok(());
+        }
+
+        Err(anyhow!("You must set the hook url via .set_url"))
+    }
+
+    pub async fn send_raw(&self, data: &WebhookMessage) -> Result<()> {
+        if let Some(url) = &self.hook_url {
+            let body = stringify_struct(&data)?;
+
+            HTTP.post(url)
+                .header("Content-Type", "application/json")
+                .body(body)
+                .send()
+                .await?;
+            return Ok(());
+        }
+
+        Err(anyhow!("You must set the hook url via .set_url"))
+    }
 }
--- tests/functions.rs
+++ tests/functions.rs
@@ -1,10 +1,26 @@
-use discord_webhook::DiscordWebhook;
+use discord_webhook::{DiscordWebhook, structures::WebhookMessage};
 use std::env::var;
 
 #[tokio::test]
 async fn text_test() {
-  let url = var("DISCORD_HOOK").unwrap();
-  let hook = DiscordWebhook::new(url);
+    let url = var("DISCORD_HOOK").unwrap();
+    let hook = DiscordWebhook::new(url);
 
-  hook.send_text("Hello World!").await.unwrap();
+    hook.send_text("Hello World!").await.unwrap();
+}
+
+#[tokio::test]
+async fn raw_test() {
+    let url = var("DISCORD_HOOK").unwrap();
+    let hook = DiscordWebhook::new(url);
+
+    let message = WebhookMessage {
+      content: Some("Hello World!".to_owned()),
+      username: Some("This changed".to_owned()),
+      embed: None,
+      avatar_url: None,
+      allowed_mentions: None,
+    };
+
+    hook.send_raw(&message).await.unwrap();
 }
